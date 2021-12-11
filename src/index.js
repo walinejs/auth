ad7b0d536dbcf3f4f1a8d42fa0f9876cdfd5fc02
lizheming <i@imnerd.org>
2021-12-11 21:45:27 +0800

    feat: get twitter user info

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ exports.github = require('./github');
 exports.weibo = require('./weibo');
 exports.twitter = require('./twitter');
 exports.facebook = require('./facebook');
+exports.google = require('./google');
 exports.qq = require('./qq');
--- src/twitter.js
+++ src/twitter.js
@@ -2,12 +2,15 @@ const Base = require('./base');
 const crypto = require('crypto');
 const qs = require('querystring');
 const OAuth = require('oauth-1.0a');
+const oauthSign = require('oauth-sign');
+const uuid = require('uuid');
 const Storage = require('./utils/storage/leancloud');
 const request = require('request-promise-native');
 
 const REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token';
 const OAUTH_URL = 'https://api.twitter.com/oauth/authorize';
 const ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token';
+const USER_INFO_URL = 'https://api.twitter.com/1.1/account/verify_credentials.json';
 
 const {TWITTER_ID, TWITTER_SECRET} = process.env;
 module.exports = class extends Base {
@@ -50,6 +53,43 @@ module.exports = class extends Base {
     return qs.parse(resp);
   }
 
+  async getUserInfoByToken({ oauth_token, oauth_token_secret }) {
+    const url = USER_INFO_URL;
+    const consumerKey = TWITTER_ID;
+    const consumerSecretKey = TWITTER_SECRET;
+  
+    const oauthToken = oauth_token;
+    const oauthTokenSecret = oauth_token_secret;
+  
+    const timestamp = Date.now() / 1000;
+    const nonce = uuid.v4().replace(/-/g, '');
+  
+    const params = {
+      include_email: true,
+      oauth_consumer_key: consumerKey,
+      oauth_nonce: nonce,
+      oauth_signature_method: 'HMAC-SHA1',
+      oauth_timestamp: timestamp,
+      oauth_token: oauthToken,
+      oauth_version: '1.0'
+    };
+  
+    params.oauth_signature = oauthSign.hmacsign('GET', url, params, consumerSecretKey, oauthTokenSecret);
+  
+    const auth = Object.keys(params).sort().map(function (k) {
+      return k + '="' + oauthSign.rfc3986(params[k]) + '"';
+    }).join(', ');
+  
+    const resp = await request({
+      url: url + '?include_email=true',
+      headers: {
+        Authorization: 'OAuth ' + auth
+      },
+      json: true
+    });
+    return resp;
+  }
+
   async redirect() {
     const {redirect, state} = this.ctx.params;
     const redirectUrl = this.getCompleteUrl('/twitter') + '?' + qs.stringify({redirect, state});
@@ -85,10 +125,16 @@ module.exports = class extends Base {
       return this.ctx.redirect(redirect + (redirect.includes('?') ? '&' : '?') + qs.stringify({oauth_verifier, oauth_token}));
     }
 
-    const userInfo = await this.getAccessToken({oauth_verifier, oauth_token});
+    // { oauth_token, oauth_token_secret, user_id, screen_name }
+    this.ctx.type = 'json';
+    const accessTokenInfo = await this.getAccessToken({oauth_verifier, oauth_token});
+    const userInfo = await this.getUserInfoByToken(accessTokenInfo);
     return this.ctx.body = {
-      user_id: userInfo.user_id,
-      screen_name: userInfo.screen_name
+      id: userInfo.id,
+      name: userInfo.name,
+      email: userInfo.email,
+      url: userInfo.url,
+      avatar: userInfo.profile_image_url_https,
     };
   }
 };
