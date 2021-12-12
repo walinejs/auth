94656ccda613bd13cf92637c6c3bfb2e1feeb56b
lizheming <i@imnerd.org>
2021-12-12 14:37:13 +0800

    chore: fix data undefined bug

--- src/facebook.js
+++ src/facebook.js
@@ -40,7 +40,7 @@ module.exports = class extends Base {
       name: user.name,
       email: user.email,
       url: user.link,
-      avatar: user.picture.data.url,
+      avatar: user.picture?.data?.url || user.picture,
     }
   }
 
