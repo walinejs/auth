2615a93242c0143f7bfc2901dc35b5654512a587
lzctuhao <lzctuhao@outlook.com>
2026-02-21 20:19:37 +0800

    Testing Huawei auth.

--- src/huawei.js
+++ src/huawei.js
@@ -10,59 +10,6 @@ const { HUAWEI_ID, HUAWEI_SECRET } = process.env;
 
 module.exports = class extends Base {
 
-  static check() {
-    return HUAWEI_ID && HUAWEI_SECRET;
-  }
-
-  static info() {
-    return {
-      origin: new URL(OAUTH_URL).hostname
-    };
-  }
-
-  /**
-   * Exchange code for token
-   * Huawei requires POST x-www-form-urlencoded
-   */
-  async getAccessToken(code) {
-
-    return request.post({
-      url: ACCESS_TOKEN_URL,
-      form: {
-        grant_type: 'authorization_code',
-        code,
-        client_id: HUAWEI_ID,
-        client_secret: HUAWEI_SECRET,
-        redirect_uri: this.getCompleteUrl('/huawei')
-      },
-      json: true
-    });
-  }
-
-  /**
-   * Huawei user info comes from id_token (JWT)
-   */
-  async getUserInfoByToken({ access_token, id_token }) {
-
-    if (!id_token) {
-      throw new Error('Missing id_token');
-    }
-
-    const decoded = jwtDecode(id_token);
-
-    return this.formatUserResponse({
-      id: decoded.sub,
-      name: decoded.name || decoded.email || decoded.sub,
-      email: decoded.email || undefined,
-      url: undefined,
-      avatar: decoded.picture || undefined,
-      originalResponse: decoded
-    }, 'huawei');
-  }
-
-  /**
-   * Redirect to Huawei OAuth
-   */
   async redirect() {
 
     const { redirect, state } = this.ctx.params;
@@ -84,6 +31,41 @@ module.exports = class extends Base {
       });
 
     return this.ctx.redirect(url);
+  }
+
+  async getAccessToken(code) {
+
+    const { redirect, state } = this.ctx.params;
+
+    const redirectUrl =
+      this.getCompleteUrl('/huawei') +
+      '?' +
+      qs.stringify({ redirect, state });
+
+    return request.post({
+      url: ACCESS_TOKEN_URL,
+      form: {
+        grant_type: 'authorization_code',
+        code,
+        client_id: HUAWEI_ID,
+        client_secret: HUAWEI_SECRET,
+        redirect_uri: redirectUrl
+      },
+      json: true
+    });
+  }
+
+  async getUserInfoByToken({ id_token }) {
+
+    const decoded = jwtDecode(id_token);
+
+    return this.formatUserResponse({
+      id: decoded.sub,
+      name: decoded.name || decoded.email || decoded.sub,
+      email: decoded.email,
+      avatar: decoded.picture,
+      originalResponse: decoded
+    }, 'huawei');
   }
 
 };
