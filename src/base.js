e046d5b2f2fe68ceafff6f840dbdbdbbdaf1f48f
lzctuhao <lzctuhao@outlook.com>
2026-02-22 19:04:42 +0800

    add debug

--- src/base.js
+++ src/base.js
@@ -1,10 +1,25 @@
-const { createUserResponse } = require('./utils');
+// base.js
+const qs = require('querystring');
+const { createErrorResponse, createUserResponse } = require('./utils');
 const storage = require('./utils/storage/db');
 
-module.exports = class {
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
+module.exports = class {
   constructor(ctx) {
     this.ctx = ctx;
+  }
+
+  getCompleteUrl(url = '') {
+    const { SERVER_URL } = process.env;
+    const protocol =
+      this.ctx && this.ctx.header && (this.ctx.header['x-forwarded-proto'] || 'http');
+    const host = this.ctx && (this.ctx.header && this.ctx.header['x-forwarded-host'] || (this.ctx.host || ''));
+    const baseUrl = SERVER_URL || protocol + '://' + host;
+    if (!/^\//.test(url)) url = '/' + url;
+    return baseUrl + url;
   }
 
   async formatUserResponse(userInfo, platform = '') {
@@ -28,5 +43,4 @@ module.exports = class {
     return createUserResponse(userInfo, platform).get();
 
   }
-
 };
